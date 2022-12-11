daef72cda44d9ed08e982a2dca9b9e14556e278a
Georg Puntigam <darkgeorok@gmail.com>
2022-12-11 22:42:40 +0100

    added some stars

--- scripts/background_dots.js
+++ scripts/background_dots.js
@@ -3,13 +3,14 @@ function freshDot(){
     this.obj.classList.add("box");
     this.obj.style.top = (window.innerHeight * Math.random()) + 'px';
     this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
-    this.size = Math.floor(5 * Math.random()) + 7;
+    this.size = Math.floor((window.innerHeight*window.innerWidth)/900000 * Math.random()) + 2;
     this.obj.style.height =  this.size + 'px';
     this.obj.style.width = this.size + 'px';
+
     
     document.body.appendChild(this.obj);
 }
 var dot = [];
-for(var i = 0 ; i < 100; i++ ){
+for(var i = 0 ; i < 200; i++ ){
     dot.push(new freshDot());
     }
